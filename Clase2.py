e41007b0ea5887978425037053d4c455e138ede2
Mauro Curcuma <108034307+Mauro-Carcamo@users.noreply.github.com>
2023-04-20 20:03:33 -0400

    acutalizacion clase2

---
+++ Clase2.py
@@ -0,0 +1,39 @@
+print ("hola mundo")
+#TIPOS DE DATOS
+#booleano -bool- true/false
+#Entero -int-
+#Flotante -float-
+#Complejo -complex-
+#Cadena -sting-
+#Tupla 
+#Lista
+#Conjunto
+#Diccionario 
+
+# VARIABLES
+# nombre = valor
+# Solo pueden contener 
+
+edad = 50
+sueldo = 1000.50
+nombre = "Richar"
+isTeacher = True
+print (edad,sueldo,nombre,isTeacher)
+
+
+dia = "nublado"
+lluvia = False
+temp_c = 34.3
+presionAtmos = 2000
+
+print(dia,lluvia,temp_c,presionAtmos)
+
+#FUNCIONES BUIL INT
+# Print ()
+# type ()
+# id ()
+
+numero_1 = 1
+nuemero_2 = 2
+resultado = (numero_1 * nuemero_2)
+print(resultado)
